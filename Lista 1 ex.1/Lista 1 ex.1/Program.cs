f7e00c7683b7c4aedde6d600185fc49dea1733cd
GluanReis <gabrielluan64@icloud.com>
2026-05-07 21:51:59 -0300

    Add files via upload

---
+++ Lista 1 ex.1/Lista 1 ex.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_1_ex._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int h;
+            int a;
+
+            Console.WriteLine("insira o valor da base");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("insira o valor de altura");
+                h = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("o valor da área é");
+            a = b * h;
+            Console.WriteLine(a);
+
+               
+
+        }
+    }
+}
